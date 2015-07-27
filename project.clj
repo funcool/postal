720064dff154422f766f76e8e323702669056714
Andrey Antukh <niwi@niwi.nz>
2015-07-27 17:24:09 +0300

    Change project name to postal.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject funcool/postal-parser "0.1.0-alpha1"
-  :description "A POSTAL protocol frames parser for Clojure and ClojureScript"
-  :url "http://github.com/funcool/postal-parser"
+(defproject funcool/postal "0.1.0-SNAPSHOT"
+  :description "A parser, renderer and validation layer for POSTAL protocol for Clojure and ClojureScript."
+  :url "http://github.com/funcool/postal"
   :license {:name "BSD (2-Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :source-paths ["src"]
