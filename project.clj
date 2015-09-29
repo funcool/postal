deaae927dc32a20547b2a3d9928ef78bd3042310
Andrey Antukh <niwi@niwi.nz>
2015-09-29 18:16:50 +0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,13 +1,12 @@
 (defproject funcool/postal "0.2.0-SNAPSHOT"
   :description "A parser, renderer and validation layer for POSTAL protocol for Clojure and ClojureScript."
   :url "http://github.com/funcool/postal"
-  :license {:name "BSD (2-Clause)"
-            :url "http://opensource.org/licenses/BSD-2-Clause"}
+  :license {:name "Public Domain"
+            :url "http://unlicense.org/"}
   :source-paths ["src"]
   :jar-exclusions [#"\.swp|\.swo|user.clj"]
   :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options"]
   :plugins [[lein-ancient "0.6.7"]]
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
                  [org.clojure/clojurescript "1.7.122" :scope "provided"]
-                 [org.clojure/tools.reader "0.10.0-alpha3"]
-                 [funcool/cuerdas "0.6.0"]])
+                 [funcool/beicon "0.1.0-SNAPSHOT"]])
