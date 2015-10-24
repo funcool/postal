eb216b87408562594f639bab5bff0e5e67301cb3
Andrey Antukh <niwi@niwi.nz>
2015-10-24 10:42:49 +0300

    Add more tests.

--- test/postal/client_tests.cljs
+++ test/postal/client_tests.cljs
@@ -2,7 +2,9 @@
   (:require [cljs.test :as t]
             [cats.core :as m]
             [promesa.core :as p]
-            [postal.client :as pc])
+            [postal.client :as pc]
+            [httpurr.errors :as e]
+            [httpurr.client :as http])
   (:import [goog.testing.net XhrIo]
            [goog.net]))
 
@@ -38,6 +40,41 @@
       (t/is (= (:method req) "PUT"))
       (t/is (= (:uri req) (:url c)))
       (t/is (not (empty? (:headers req)))))))
+
+
+(t/deftest send-query-that-fails-with-timeout-spec
+  (t/async done
+    (let [c (pc/client "http://localhost/api")
+          r (pc/query c :users {:id 1})]
+      (p/catch r (fn [err]
+                   (t/is (= err e/timeout))
+                   (done))))
+    (let [xhr (raw-last-request)]
+      (.simulateTimeout xhr))))
+
+(t/deftest send-novelty-that-is-aborted-spec
+  (t/async done
+    (let [c (pc/client "http://localhost/api")
+          r (pc/query c :users {:id 1})]
+      (p/catch r (fn [err]
+                   (t/is (= err e/abort))
+                   (done)))
+      (http/abort! r))))
+
+(t/deftest send-novelty-and-recv-response-spec
+  (t/async done
+    (let [rframe (pc/encode {:type :response :data [1]})]
+      (let [c (pc/client "http://localhost/api")
+            r (pc/novelty c :users {:id 1})]
+        (p/then r (fn [{:keys [type data]}]
+                    (t/is (= type :response))
+                    (t/is (= data [1]))
+                    (done))))
+      (let [xhr (raw-last-request)
+            status 200
+            body rframe
+          headers #js {"Content-Type" "application/transit+json"}]
+        (.simulateResponse xhr status body headers)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Initial Setup & Entry Point
