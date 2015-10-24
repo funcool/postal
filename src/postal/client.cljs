6a23bcae236d6a1f35bd469b784d7926c2a89418
Andrey Antukh <niwi@niwi.nz>
2015-10-24 18:27:10 +0300

    Add subscription pattern using eventsource.

--- src/postal/client.cljs
+++ src/postal/client.cljs
@@ -1,11 +1,13 @@
 (ns postal.client
   (:require [cognitect.transit :as t]
             [promesa.core :as p]
-            [cats.core :as m]
+            [beicon.core :as s]
             [goog.crypt.base64 :as b64]
             [httpurr.client :as http]
             [httpurr.status :as http-status]
-            [httpurr.client.xhr :as xhr]))
+            [httpurr.client.xhr :as xhr])
+  (:import goog.Uri.QueryData
+           goog.Uri))
 
 (def ^:private
   +default-headers+
@@ -39,26 +41,40 @@
     (p/rejected (ex-info "Unexpected" response))))
 
 (defn- prepare-request
-  [client data]
-  (let [req {:headers (merge +default-headers+ @(:headers client))
-             :method (:method client)
-             :url (:url client)}]
-    (if (= :method :get)
-      (merge req {:query-string (str "d=" (b64/encodeString data true))})
-      (merge req {:body data}))))
+  ([client data]
+   (prepare-request client data (:method client) {}))
+  ([client data method params]
+   (let [req {:headers (merge +default-headers+ @(:headers client))
+              :method method
+              :url (:url client)}]
+     (if (= method :get)
+       (let [pd (.clone (:params client))]
+         (when-not (empty? params)
+           (.extend pd (clj->js params)))
+         (.set pd "d" (b64/encodeString data true))
+         (merge req {:query-string (.toString pd)}))
+       (if (empty? params)
+         (let [pd (:params client)]
+           (merge req {:body data :query-string (.toString pd)}))
+         (let [pd (.clone (:params client))]
+           (.extend pd (clj->js params))
+           (merge req {:body data :query-string (.toString pd)})))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; The basic client interface.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defrecord Client [headers method url])
+(defrecord Client [headers method url params])
 
 (defn client
   "Creates a new client instance from socket."
   ([url]
    (client url {}))
-  ([url {:keys [headers method] :or {headers {} method :put}}]
-   (Client. (atom headers) method url)))
+  ([url {:keys [headers method params]
+         :or {headers {} method :put params {}}}]
+   (let [paramsdata (QueryData.)]
+     (.extend paramsdata (clj->js params))
+     (Client. (atom headers) method url paramsdata))))
 
 (defn client?
   "Return true if a privided client is instance
@@ -91,6 +107,10 @@
     (-> (http/send! xhr/client req)
         (p/then process-response))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Reques/Reply Pattern
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn query
   "Sends a query message to a server."
   ([client dest]
@@ -114,3 +134,35 @@
                          :dest dest
                          :data data}
                         opts))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; EventSource
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defprotocol ISubscription
+  (-stream [_] "Get the stream")
+  (-close [_] "Close subscription"))
+
+(deftype Subscription [bus closed evs]
+  (-stream [_]
+    (s/to-observable bus))
+  (-close [_]
+    (.close evs)
+    (s/end! bus)))
+
+(defn subscribe
+  [client dest data {:keys [params] :or {params {}}}]
+  (let [req (prepare-request client data :get params)
+        uri (Uri. (:url req))
+        bus (s/bus)]
+    (.setQuery uri (:query-string req))
+    (letfn [(on-message [event]
+              (let [message (decode (.-data event))]
+                (s/push bus message)))
+            (on-error [event]
+              (s/error! bus event))]
+      (let [evs (js/EventSOurce (.toString uri))]
+        (.addEventListener evs "open" on-open)
+        (.addEventListener evs "message" on-message)
+        (.addEventListener evs "error" on-error)
+        (Subscription. bus (atom false) evs)))))
