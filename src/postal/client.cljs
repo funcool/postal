d272e763436ceb6c0a3d9b56f9631b364de316a9
Andrey Antukh <niwi@niwi.nz>
2015-11-01 14:26:24 +0200

    Fix bugs on socket and subscribe functions.

--- src/postal/client.cljs
+++ src/postal/client.cljs
@@ -139,7 +139,7 @@
                         opts))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; EventSource
+;; Socket
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn socket
@@ -147,22 +147,26 @@
    (socket client dest nil nil))
   ([client dest data]
    (socket client dest data nil))
-  ([client dest data {:keys [params] :or {params {}}}]
-   (let [req (prepare-request client data :get params)
+  ([client dest data {:keys [params _type] :or {params {} _type :socket}}]
+   (let [frame (encode {:data data :dest dest :type _type})
+         req (prepare-request client frame :get params)
          uri (Uri. (:url req))]
      (.setQuery uri (:query-string req))
      (.setScheme uri (if (= (.getScheme uri) "http") "ws" "wss"))
-     (let [ws (WebSocket. (.toString uri))
+     (let [ws (WebSocket. false)
            timer (Timer. 5000)
            busin (s/bus)
            streamin (s/filter #(not= (:type %) :ping) busin)
            busout (s/bus)]
        (letfn [(on-ws-message [event]
-                 (let [frame (decode (.-data event))]
+                 (let [frame (decode (.-message event))]
                    (s/push! busin frame)))
                (on-ws-error [event]
                  (s/error! busin event)
                  (.close ws))
+               (on-ws-closed [event]
+                 (s/end! busout)
+                 (s/end! busin))
                (on-timer-tick [_]
                  (let [frame {:type :ping}]
                    (s/push! busout frame)))
@@ -171,12 +175,17 @@
                    (.send ws data)))
                (on-busout-end []
                  (.close ws))]
+
          (s/on-end busout on-busout-end)
          (s/on-value busout on-busout-value)
 
          (events/listen ws EventType.MESSAGE on-ws-message)
          (events/listen ws EventType.ERROR on-ws-error)
+         (events/listen ws EventType.CLOSED on-ws-closed)
          (events/listen timer Timer.TICK on-timer-tick)
+
+         (.start timer)
+         (.open ws (.toString uri))
 
          [streamin busout])))))
 
@@ -186,7 +195,7 @@
   ([client dest data]
    (subscribe client dest data nil))
   ([client dest data {:keys [params] :or {params {}} :as opts}]
-   (let [[in out] (socket client dest data opts)
+   (let [[in out] (socket client dest data (assoc opts :_type :subscribe))
          bus (s/bus)]
      (s/subscribe in #(s/push! bus %) #(s/error! bus %) #(s/end! bus))
      (s/on-end bus #(s/end! out))
