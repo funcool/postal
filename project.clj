c7ccd08429006d06773c99bdaff861b497e9e51b
Andrey Antukh <niwi@niwi.nz>
2016-01-08 20:36:24 +0200

    Update dependencies.

--- project.clj
+++ project.clj
@@ -9,8 +9,8 @@
   :plugins [[lein-ancient "0.6.7"]]
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
                  [org.clojure/clojurescript "1.7.189" :scope "provided"]
-                 [com.cognitect/transit-cljs "0.8.232"]
-                 [funcool/cats "1.2.0"]
-                 [funcool/httpurr "0.2.0"]
-                 [funcool/beicon "0.2.0"]
-                 [funcool/promesa "0.6.0"]])
+                 [com.cognitect/transit-cljs "0.8.237"]
+                 [funcool/cats "1.2.1"]
+                 [funcool/httpurr "0.3.0"]
+                 [funcool/beicon "0.5.1"]
+                 [funcool/promesa "0.7.0"]])
