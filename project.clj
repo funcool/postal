3098056360a729f667c62bf80a70a364efd863f2
Andrey Antukh <niwi@niwi.nz>
2016-01-23 19:33:42 +0200

    Upgrade dependnecies.

--- project.clj
+++ project.clj
@@ -7,10 +7,10 @@
   :jar-exclusions [#"\.swp|\.swo|user.clj"]
   :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options"]
   :plugins [[lein-ancient "0.6.7"]]
-  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.7.189" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.7.228" :scope "provided"]
                  [com.cognitect/transit-cljs "0.8.237"]
                  [funcool/cats "1.2.1"]
                  [funcool/httpurr "0.3.0"]
-                 [funcool/beicon "0.5.1"]
+                 [funcool/beicon "0.6.0"]
                  [funcool/promesa "0.7.0"]])
