e47ea4a217a460dccb428982e3c93acf10c5348f
Andrey Antukh <niwi@niwi.nz>
2016-03-19 10:52:51 +0200

    Update dependencies.

--- project.clj
+++ project.clj
@@ -7,8 +7,7 @@
   :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options"]
   :plugins [[lein-ancient "0.6.7"]]
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.7.228" :scope "provided"]
+                 [org.clojure/clojurescript "1.8.34" :scope "provided"]
                  [com.cognitect/transit-cljs "0.8.237"]
                  [funcool/httpurr "0.4.0"]
-                 [funcool/beicon "0.6.1"]
-                 [funcool/promesa "0.8.1"]])
+                 [funcool/beicon "1.1.0"]])
