98937a6ce393df03029fcac7b633e3c8358a5db8
Andrey Antukh <niwi@niwi.nz>
2016-05-26 21:16:46 +0300

    Update dependencies.

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Public Domain" :url "http://unlicense.org/"}
   :source-paths ["src"]
   :jar-exclusions [#"\.swp|\.swo|user.clj"]
-  :plugins [[lein-ancient "0.6.7"]]
+  :plugins [[lein-ancient "0.6.10"]]
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.8.34" :scope "provided"]
+                 [org.clojure/clojurescript "1.8.51" :scope "provided"]
                  [com.cognitect/transit-cljs "0.8.237"]
-                 [funcool/httpurr "0.4.0"]
-                 [funcool/beicon "1.1.0"]])
+                 [funcool/httpurr "0.6.0"]
+                 [funcool/beicon "1.3.0"]])
